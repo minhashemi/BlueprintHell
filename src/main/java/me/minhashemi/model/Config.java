3865ef6aefa37894a9269546c09b749aef8550ba
Amin Hashemi <hi@aminhashemi.dev>
2025-05-06 09:56:59 +0330

    added main menu
    theme song
    main menu

---
+++ src/main/java/me/minhashemi/model/Config.java
@@ -0,0 +1,10 @@
+package me.minhashemi.model;
+
+public class Config {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int FPS = 60;
+    public static boolean isMusicOn = true;
+    public static boolean recordTime = false;
+    public static int lastPlayedStage = 1;
+}
