a8c7d4472798d6f4c78acfde2f088d3e1d88926b
Amin Hashemi <52834735+minhashemi@users.noreply.github.com>
2025-05-09 18:58:44 +0330

    integrate levels w/json

--- src/main/java/me/minhashemi/model/Config.java
+++ src/main/java/me/minhashemi/model/Config.java
@@ -1,10 +1,16 @@
 package me.minhashemi.model;
 
 public class Config {
+    // screen
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final int FPS = 60;
     public static boolean isMusicOn = true;
     public static boolean recordTime = false;
     public static int lastPlayedStage = 1;
+
+    // packet
+    public static final int PORT_SIZE = 10;
+    public static final int STANDARD_HEIGHT = 30;
+    public static final int PACKET_WIDTH = 60;
 }
---
+++ src/main/java/me/minhashemi/model/Position.java
@@ -0,0 +1,6 @@
+package me.minhashemi.model;
+
+public class Position {
+    public int x;
+    public int y;
+}
