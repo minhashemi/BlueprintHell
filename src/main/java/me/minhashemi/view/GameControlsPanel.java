b1c5052d8ce2eeedcbc07be5205d3fd9db041755
Amin Hashemi <52834735+minhashemi@users.noreply.github.com>
2025-05-12 13:43:12 +0330

    time, quit buttons in gamescreen

---
+++ src/main/java/me/minhashemi/view/GameControlsPanel.java
@@ -0,0 +1,29 @@
+package me.minhashemi.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameControlsPanel extends JPanel {
+    public interface GameControlListener {
+        void onTimeForward();
+        void onTimeBackward();
+        void onQuitToMenu();
+    }
+
+    public GameControlsPanel(GameControlListener listener) {
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setOpaque(false);
+
+        JButton forwardBtn = new JButton("⏩ Time +");
+        JButton backBtn = new JButton("⏪ Time -");
+        JButton quitBtn = new JButton("🚪 Quit");
+
+        forwardBtn.addActionListener(e -> listener.onTimeForward());
+        backBtn.addActionListener(e -> listener.onTimeBackward());
+        quitBtn.addActionListener(e -> listener.onQuitToMenu());
+
+        add(forwardBtn);
+        add(backBtn);
+        add(quitBtn);
+    }
+}
