22e9af866a90fc84ebdef68f1436c04d742ac5b7
Amin Hashemi <hi@aminhashemi.dev>
2025-05-20 15:24:15 +0330

    shop overlay fixed

--- src/main/java/me/minhashemi/model/shop/items/ResetNoiseEffect.java
+++ src/main/java/me/minhashemi/model/shop/items/ResetNoiseEffect.java
@@ -3,9 +3,12 @@ package me.minhashemi.model.shop.items;
 import me.minhashemi.model.shop.ShopEffect;
 import me.minhashemi.view.GameScreen;
 import me.minhashemi.model.MovingPacket;
+import me.minhashemi.view.HUD;
+import me.minhashemi.model.shop.ShopItem;
 
 public class ResetNoiseEffect implements ShopEffect {
     private GameScreen gameScreen = null;
+    private HUD hud;
 
     public ResetNoiseEffect() {
         this.gameScreen = gameScreen;
@@ -13,6 +16,10 @@ public class ResetNoiseEffect implements ShopEffect {
 
     @Override
     public void applyEffect() {
+        if (HUD.getCoins() < ShopItem.getPrice()){
+            hud.showCallout("🪙 Not enough coins");
+            return;
+        }
         for (MovingPacket packet : gameScreen.getMovingPackets()) {
             packet.setNoise(0);
         }
